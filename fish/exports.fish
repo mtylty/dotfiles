5fa4f8369d169b44e15f2515f534cba38026a4fd
Matteo Latini <mtylty@gmail.com>
2013-11-06 10:26:36 +0100

    remembers colors

--- fish/exports.fish
+++ fish/exports.fish
@@ -29,3 +29,30 @@ set -x LESS_TERMCAP_se (set_color normal)
 set -x LESS_TERMCAP_so (set_color -b blue -o yellow)
 set -x LESS_TERMCAP_ue (set_color normal)
 set -x LESS_TERMCAP_us (set_color -o green)
+
+# Fish specific configuration
+set -e fish_greeting
+set -g fish_color_autosuggestion 555\x1eyellow
+set -g fish_color_command 005fd7
+set -g fish_color_comment red
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_error red\x1e\x2d\x2dbold
+set -g fish_color_escape cyan
+set -g fish_color_history_current cyan
+set -g fish_color_host ff8700
+set -g fish_color_match cyan
+set -g fish_color_normal normal
+set -g fish_color_operator cyan
+set -g fish_color_param 00afff\x1ecyan
+set -g fish_color_quote brown
+set -g fish_color_redirection normal
+set -g fish_color_search_match \x2d\x2dbackground\x3dpurple
+set -g fish_color_status red
+set -g fish_color_user d7005f
+set -g fish_color_valid_path \x2d\x2dunderline
+set -g fish_key_bindings fish\x5fdefault\x5fkey\x5fbindings
+set -g fish_pager_color_completion normal
+set -g fish_pager_color_description 555\x1eyellow
+set -g fish_pager_color_prefix cyan
+set -g fish_pager_color_progress cyan
