1b1e2da13caa7094fc8a9f831fe96df5805f7897
Matteo Latini <mtylty@gmail.com>
2014-03-20 10:30:07 +0100

    removes boxen from fish

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,3 @@ set -x FISH_HOME "$HOME/.config/fish"
 
 # Aliases
 [ -r "$FISH_HOME/aliases.fish" ]; and source "$FISH_HOME/aliases.fish"
-
-# Boxen environment
-[ -r "$FISH_HOME/boxen.fish" ]; and source "$FISH_HOME/boxen.fish"
