343ebe118650d1e18baf77cfcea243b6434efb2b
Matteo Latini <matteolatini@nebulab.it>
2020-07-17 22:48:33 +0200

    Update aliases

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -2,13 +2,9 @@
 alias be "bundle exec"
 alias bi "bundle install"
 alias bo "bundle open"
-alias rubocop_pr "git fetch; and git diff-tree -r --no-commit-id --name-only master@\{u\} head | xargs ls -1 2>/dev/null | grep '\.rb\$' | xargs rubocop"
 
 # Console aliases
 alias g "git"
-alias gd "git diff"
-alias gs "git status"
-alias gs "git status"
 alias l "ls"
 alias la "ls -la"
 alias ll "ls -l"
